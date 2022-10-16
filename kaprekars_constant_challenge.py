9316b1086b820dcd7597861bce0a0aaac59193a4
Ryan-Kethley <113642502+Ryan-Kethley@users.noreply.github.com>
2022-10-15 20:35:20 -0500

    Delete kaprekars_constant_challenge.py

--- kaprekars_constant_challenge.py
+++
@@ -1,8 +0,0 @@
-# By submitting this assignment, I agree to the following:
-#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
-#   "I have not given or received any unauthorized aid on this assignment."
-#
-# Name:         Ryan Kethley
-# Section:      565
-# Assignment:   7.29 LAB: Kaprekar's Constant Challenge
-# Date:         13 October 2022
